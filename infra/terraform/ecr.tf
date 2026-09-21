2c56562aff409afd5dfca7d96c474928093ca510
SamuelCagnani <samuelcagnani@gmail.com>
2026-09-21 13:48:42 -0300

    fix(infra): force delete ECR repositories on destroy
    Repositories with images blocked terraform destroy; force_delete lets a
    single destroy clean the environment and avoid leftover S3 storage cost.

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -3,6 +3,8 @@
 resource "aws_ecr_repository" "reservation" {
   name                 = "${var.project}/reservation-service"
   image_tag_mutability = "MUTABLE"
+  # Permite remover o repositorio (e as imagens) no terraform destroy.
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -12,6 +14,8 @@ resource "aws_ecr_repository" "reservation" {
 resource "aws_ecr_repository" "match" {
   name                 = "${var.project}/match-service"
   image_tag_mutability = "MUTABLE"
+  # Permite remover o repositorio (e as imagens) no terraform destroy.
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
